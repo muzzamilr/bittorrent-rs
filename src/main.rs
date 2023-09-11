1a85a5a368fb93859cea19e2069556e067b319ac
muzzamilr <mmuzzamil90@gmail.com>
2023-09-11 17:24:31 +0500

    complete stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,29 @@
 use serde_bencode::{from_str, value::Value};
-use serde_json;
 use std::env;
 
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    let start = encoded_value.chars().next().unwrap();
-    if start == 'l' {}
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "5"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value.chars().next().unwrap() == 'i' {
-        let e_index = encoded_value.find('e').unwrap();
-        let number = encoded_value[1..e_index].parse::<i64>().unwrap();
-        return serde_json::Value::Number(number.into());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
-}
 
 fn decode(value: &Value) -> String {
     match value {
         Value::Int(i) => i.to_string(),
         Value::List(l) => format!(
             "[{}]",
-            l.iter()
-                .map(|v| decode(v))
-                .collect::<Vec<String>>()
-                .join(",")
+            l.iter().map(decode).collect::<Vec<String>>().join(",")
         ),
         Value::Bytes(b) => format!("{:?}", String::from_utf8(b.clone()).unwrap()),
-        _ => panic!("Unhandled encoded value: {:?}", value),
+        Value::Dict(d) => {
+            let mut result: Vec<String> = Vec::new();
+            for (key, value) in d {
+                let key_str = String::from_utf8_lossy(key).to_string();
+                let val = decode(value);
+                result.push(format!("\"{}\":{}", key_str, val));
+            }
+            result.sort();
+            format!("{{{}}}", result.join(","))
+        }
     }
 }
 
@@ -52,9 +38,7 @@ fn main() {
 
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
         let value = from_str::<Value>(encoded_value).unwrap();
-        // println!("{}", decoded_value.to_string());
         println!("{}", decode(&value));
     } else {
         println!("unknown command: {}", args[1])
